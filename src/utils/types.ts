f8fac0bbdaab12a979b6d913940f67d031fd2521
joesobo <soboleskijoe@gmail.com>
2023-04-21 16:07:35 -0700

    convert to type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,25 +1,25 @@
 import { EdgeOptions, NodeOptions } from 'vis-network/standalone'
 
-export interface Node extends NodeOptions {
-  id: number
-  label: string
+export type Node = NodeOptions & {
+  id: number;
+  label: string;
+  path: string;
+  type: 'File' | 'Directory' | 'Package';
+  color?: string;
+};
+
+export type Edge = EdgeOptions & {
+  id: string;
+  to: number;
+  from: number;
+  color?: string;
+};
+
+export type File = {
   path: string
-  type: 'File' | 'Directory' | 'Package'
-  color?: string
 }
 
-export interface Edge extends EdgeOptions {
-  id: string
-  to: number
-  from: number
-  color?: string
-}
-
-export interface File {
-  path: string
-}
-
-export interface Directory {
+export type Directory = {
   path: string
 }
 
